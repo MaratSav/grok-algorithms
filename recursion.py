fb669717412dcbf0479741befc7f9a75096ebfc9
MaratSav <marat1620@gmail.com>
2024-12-08 14:49:43 +0300

    рекурсия

--- recursion.py
+++ recursion.py
@@ -8,11 +8,40 @@ def look_for_key(main_box):
             elif item.is_a_key():
                 print("found the key")
 
-def look_for_key(box):
+def look_for_key1(box):
     for item in box:
         if item.is_a_box():
-            look_for_key(item)  # Рекурсивный вызов для вложенной коробки
+            look_for_key1(item)  # Рекурсивный вызов для вложенной коробки
         elif item.is_a_key():
             print("found the key")
 
+def countdown(i):  # Обратный отсчет
+    print(i)
+    if i <= 1:  # Базовый случай
+        return
+    else:   # Рекурсивный случай
+        countdown(i-1)
+countdown(5)
+
+def greet():
+    print("Hello, " + name + "!")
+def question():
+    print("how are you " + name + "?")
+def bye():
+    print("ok bye " + name)
+
+name = "Alex"
+greet()
+question()
+bye()
+
+# Факториал
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x-1)
+x = 4
+print(fact(x))
+print(fact(5))
 
